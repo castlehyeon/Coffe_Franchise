08863a9688407e9d6d68b41f1c2f18f9f17ecb9d
devsibong <seohae7785@gmail.com>
2023-04-12 15:13:28 +0900

    Feat : Member Class 생성
    - abstract class Member 생성
    - StoreAdmin class 생성
    - Customer class 생성
    - StoreOwner class 생성

---
+++ Coffe_Franchise/src/member/Customer.java
@@ -0,0 +1,13 @@
+package member;
+
+import java.util.List;
+
+public class Customer extends Member {
+	private int gifticon;
+	private int stamp;
+	private List<Order> orders;
+	
+	public void orderMenu() {}
+	public void savsStamp() {}
+	public void createGifticon() {}
+}
---
+++ Coffe_Franchise/src/member/StoreAdmin.java
@@ -0,0 +1,12 @@
+package member;
+
+public class StoreAdmin extends Member {
+	private DuzoneCoffee duzoneCoffee;
+	
+	public void createMenu() {}
+	public void updateMenu() {}
+	public void createStore() {}
+	public void updateStore() {}
+	public void deleteStore() {}
+	public void storeSalesList() {}
+}
