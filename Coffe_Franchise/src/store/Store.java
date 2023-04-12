610c2e40c5e8caf0972619aeb565bde6e4f6ba11
hyunsohyun <llikepsh515@gmail.com>
2023-04-12 20:21:30 +0900

    add headquarter function

--- Coffe_Franchise/src/store/Store.java
+++ Coffe_Franchise/src/store/Store.java
@@ -1,24 +1,77 @@
 package store;
 
-import java.util.List;
-
-import menu.Menu;
-
 public class Store extends Headquarter{
 	
-	public String storeCode;	//가게코드
-	public String name;			//지점이름
-	public String address;		//주소
-	public String phoneNumber;	//핸드폰번호
-	public int totalSales;		//총 매출
+	private int storeCode;		//가게코드
+	private String name;		//지점이름
+	private String address;		//주소
+	private String phoneNumber;	//핸드폰번호
+	private int totalSales;		//총 매출
 	
-	public Store(String storeCode, String name, String address, String phoneNumber, int totalSales) {
-		this.storeCode = storeCode;
+	public Store(String name, String address, String phoneNumber) {
 		this.name = name;
 		this.address = address;
 		this.phoneNumber = phoneNumber;
+		this.storeCode = storeCode + 1;
+	}
+
+
+	public int getStoreCode() {
+		return storeCode;
+	}
+
+
+	public void setStoreCode(int storeCode) {
+		this.storeCode = storeCode;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	public int getTotalSales() {
+		return totalSales;
+	}
+
+
+	public void setTotalSales(int totalSales) {
 		this.totalSales = totalSales;
 	}
-	
+
+
+	@Override
+	public String toString() {
+		return "Store [storeCode=" + storeCode + ", name=" + name + ", address=" + address + ", phoneNumber="
+				+ phoneNumber + ", totalSales=" + totalSales + "]";
+	}
+
+
 	
 }
