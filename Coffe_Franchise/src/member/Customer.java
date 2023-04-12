57835a7d66f7f6c3bfc731ef354aa6e9bfe30666
devsibong <seohae7785@gmail.com>
2023-04-12 22:33:28 +0900

    Feat: Customer Class 추가

--- Coffe_Franchise/src/member/Customer.java
+++ Coffe_Franchise/src/member/Customer.java
@@ -8,11 +8,12 @@ import payment.Cash;
 import payment.Credit;
 import payment.Gifticon;
 import payment.Payment;
+import store.Sales;
 
 public class Customer extends Member implements NoneAdmin {
-	private int gifticon;
-	private int stamp;
-	private List<Order> orders;
+	private int gifticon;	// 기프티콘 개수
+	private int stamp;		// 스탬프 개수
+	private List<Order> orders;	// 장바구니
 	
 	// 생성자
 	Customer(String ID, String password, String memberType, String phoneNumber) {
@@ -30,12 +31,18 @@ public class Customer extends Member implements NoneAdmin {
 	}
 	
 	// 주문 리스트에 있는 메뉴의 갯수만큼 스탬프 추가
+	// 스탬프 추가는 결제 마지막에 진행
 	public void saveStamp() {
-		int addStamp = this.orders.size();
+		int addStamp = 0;
+		for(Order order: orders) {
+			addStamp += order.getMenuCount();
+		}
 		System.out.printf("현재 스탬프는 %d개 입니다.\n", this.stamp);
 		this.stamp += addStamp;
 		System.out.printf("총 %d개 적리되어 현재 스탬프는 %d개 입니다.\n", addStamp, this.stamp);
-		this.createGifticon();
+		this.createGifticon();	// 스탬프 적립 후 기프티콘으로 전환
+		this.orders = new ArrayList<Order>();	// 장바구니 초기화
+		
 	}
 	
 	// 현재 보유중인 스탬프를 모두 기프티콘을 전환
@@ -47,15 +54,19 @@ public class Customer extends Member implements NoneAdmin {
 	}
 	
 	// 결제 진행
-	public void payProducts(Payment payment) {
+	public Sales payProducts(Payment payment) {
 		int totalPrice = payment.getPaymetAmount();
 		System.out.printf("총 결제금액은 %d원 입니다.\n", totalPrice);
+		Sales sales = new Sales();	// 주문내역 생성
 		
 		// 카드결제일 때
 		if(payment instanceof Credit) {
 			System.out.println("결제 완료");
-			this.orders = new ArrayList<Order>();	// 장바구니 초기화
 			
+			sales.setMember(this);
+			sales.setOrderList(orders);
+			sales.setPayment(payment);
+			sales.setPayMoney(totalPrice);	// storeCode를 제외한 주문내역 생성
 		} 
 		
 		// 현금결제일 때
@@ -63,8 +74,11 @@ public class Customer extends Member implements NoneAdmin {
 			// 투입한 돈보다 결제금액이 작을 때
 			if(payment.getPaymetAmount() <= ((Cash) payment).getInputMoney()) {
 				System.out.println("결제 완료");
-				this.orders = new ArrayList<Order>();	// 장바구니 초기화
 				((Cash)payment).returnChange();	//거스름돈 반환
+				sales.setMember(this);
+				sales.setOrderList(orders);
+				sales.setPayment(payment);
+				sales.setPayMoney(totalPrice);
 			} else {
 				System.out.println("돈이 부족합니다.");
 			}
@@ -73,15 +87,24 @@ public class Customer extends Member implements NoneAdmin {
 		
 		// 기프티콘 결제일 때
 		else if(payment instanceof Gifticon) {
-			if(this.gifticon>=1) {
+			int menuCount = 0;
+			for(Order order: orders) {
+				menuCount += order.getMenuCount();
+			}
+			if(this.gifticon>=menuCount) {
 				System.out.println("결제 완료");
-				this.orders = new ArrayList<Order>();	// 장바구니 초기화
-				this.gifticon--;
+				this.gifticon -= menuCount;
 				System.out.printf("현재 잔여 기프티콘은 %d개 입니다.\n", gifticon);
+				sales.setMember(this);
+				sales.setOrderList(orders);
+				sales.setPayment(payment);
+				sales.setPayMoney(totalPrice);
 			} else {
 				System.out.println("잔여 기프티콘이 없습니다.");
 			}
 			
 		}
+		
+		return sales;
 	}
 }
--- Coffe_Franchise/src/store/Sales.java
+++ Coffe_Franchise/src/store/Sales.java
@@ -3,22 +3,53 @@ package store;
 import java.util.List;
 
 import member.Member;
+import member.Order;
 import menu.Menu;
 import payment.Payment;
 
 public class Sales {
 	private int storeCode;
-	private List<Menu> orderList;
+	private List<Order> orderList;
 	private Payment payment;
 	private int payMoney;
 	private Member member;
-	
-	Sales(int storeCode, List<Menu> orderList, Payment payment, int payMoney, Member member) {
+	public Sales(){}
+	public Sales(int storeCode, List<Order> orderList, Payment payment, int payMoney, Member member) {
 		this.storeCode = storeCode;
 		this.orderList = orderList;
 		this.payment = payment;
 		this.payMoney = payMoney;
 		this.member = member;
 	}
+	public int getStoreCode() {
+		return storeCode;
+	}
+	public void setStoreCode(int storeCode) {
+		this.storeCode = storeCode;
+	}
+	public List<Order> getOrderList() {
+		return orderList;
+	}
+	public void setOrderList(List<Order> orderList) {
+		this.orderList = orderList;
+	}
+	public Payment getPayment() {
+		return payment;
+	}
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+	public int getPayMoney() {
+		return payMoney;
+	}
+	public void setPayMoney(int payMoney) {
+		this.payMoney = payMoney;
+	}
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
+	
 }
-
