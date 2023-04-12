392a405997a0c0c37086c81ea0ec04208217b07d
devsibong <seohae7785@gmail.com>
2023-04-13 00:07:57 +0900

    Feat: StoreOwner 수정

--- Coffe_Franchise/src/member/StoreOwner.java
+++ Coffe_Franchise/src/member/StoreOwner.java
@@ -1,8 +1,9 @@
 package member;
 
+import store.Store;
+
 public class StoreOwner extends Member {
-	private DuzoneFranchisee duzoneFranchisee;
-	
+	private Store store;
 	public void showTotalSalesByTime() {}
 	public void showTotalClientByTime() {}
 }
