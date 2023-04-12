6e360dee3a0a5c5f1ab6f35889e6d94417df9607
castlehyeon <qnftktls118@naver.com>
2023-04-12 22:37:55 +0900

    Admin 컨트롤러 구현

--- Coffe_Franchise/src/store/HeadquarterController.java
+++ Coffe_Franchise/src/store/HeadquarterController.java
@@ -2,5 +2,7 @@ package store;
 
 public class HeadquarterController {
     public void start() {
+        Headquarter headquarter = new Headquarter();
+        headquarter.headquarterMainMenu();
     }
 }
--- Coffe_Franchise/src/store/StoreController.java
+++ Coffe_Franchise/src/store/StoreController.java
@@ -1,6 +1,7 @@
 package store;
-
+//가맹점주
 public class StoreController {
     public void start() {
+
     }
 }
