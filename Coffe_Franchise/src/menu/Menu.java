18d17748c653a33698ab062fdbf44ea418420f19
devsibong <seohae7785@gmail.com>
2023-04-12 16:40:52 +0900

    Feat : orderMenu 메서드 추가

--- Coffe_Franchise/src/menu/Menu.java
+++ Coffe_Franchise/src/menu/Menu.java
@@ -4,4 +4,23 @@ public class Menu {
 	private int menuCode;
 	private String menuName;
 	private int menuPrice;
+	public int getMenuCode() {
+		return menuCode;
+	}
+	public void setMenuCode(int menuCode) {
+		this.menuCode = menuCode;
+	}
+	public String getMenuName() {
+		return menuName;
+	}
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+	public int getMenuPrice() {
+		return menuPrice;
+	}
+	public void setMenuPrice(int menuPrice) {
+		this.menuPrice = menuPrice;
+	}
+	
 }
