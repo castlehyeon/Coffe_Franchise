ee0d324bfcbc0fdf150db0d0b075d6476a1b0704
hyunsohyun <llikepsh515@gmail.com>
2023-04-14 09:54:48 +0900

    fix changeStoreOwner

--- Coffe_Franchise/src/HeadquarterMain.java
+++ Coffe_Franchise/src/HeadquarterMain.java
@@ -1,14 +1,14 @@
-import member.AdminController;
+import global.Controller;
 
 public class HeadquarterMain {
 
 	public static void main(String[] args) {
 
-//		Controller controller = new Controller();
-//		controller.prepare();
-//		controller.start();
-		AdminController con = new AdminController();
-		con.start();
+		Controller controller = new Controller();
+		controller.prepare();
+		controller.start();
+//		AdminController con = new AdminController();
+//		con.start();
 	}
 
 }
